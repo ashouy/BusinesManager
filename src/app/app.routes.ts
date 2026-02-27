7cc3e1b3bd7c0e85b6e48e58027c1f3c8e865581
joaopaulo <joaopaulokad@gmail.com>
2026-02-27 11:18:56 -0300

    Refactor route imports to use new feature structure for components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
-import { HomePageComponent } from './pages/home/home.page';
-import { AgendaPageComponent } from './pages/agenda/agenda.page';
-import { InsumosPageComponent } from './pages/insumos/insumos.page';
-import { PacientesPageComponent } from './pages/pacientes/pacientes.page';
-import { DocumentosPageComponent } from './pages/documentos/documentos.page';
-import { RelatoriosPageComponent } from './pages/relatorios/relatorios.page';
-import { ConfiguracoesPageComponent } from './pages/configuracoes/configuracoes.page';
+import { HomePageComponent } from './features/home/home.page';
+import { AgendaPageComponent } from './features/agenda/agenda.component';
+import { InsumosPageComponent } from './features/insumos/insumos.component';
+import { PacientesPageComponent } from './features/pacientes/pacientes.component';
+import { DocumentosPageComponent } from './features/documentos/documentos.component';
+import { RelatoriosPageComponent } from './features/relatorios/relatorios.component';
+import { ConfiguracoesPageComponent } from './features/configuracoes/configuracoes.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RecoverPasswordComponent } from './auth/recover-password/recover-password.component';
 
